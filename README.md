0decb2e337d0ea5390cdf32b6df9ef5e26e8ea2a
James Martineau <James.Martineau@libertymutual.com>
2017-12-07 17:50:55 -0500

    project minimums complete. see READ.ME for instructions

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # flashcard-generator
 This is a basic flashcard application.  The backend will essentially constitute an API that allows users to create two types of flashcards.   Basic flashcards, which have a front ("Who was the first president of the United States?"), and a back ("George Washington"). Cloze-Deleted flashcards, which present partial text ("... was the first president of the United States."), and the full text when the user requests it ("George Washington was the first president of the United States.")
+
+Instructions: 
+1. set terminal directory to 'flashcard-generator' folder. 
+2. type: node main.js basic/cloze (either basic or cloze depending on the type of flashcard you want)
+3. enjoy the goodness!
+
+If you want to add flashcards, navigate to the appropriate .txt file and add your flashcard in the following format (Question, Answer;)
--- main.js
+++ main.js
@@ -1,14 +1,19 @@
+//import constructor files
 var basicCardObj = require("./basicCard.js");
 var clozeCardObj = require("./clozeCard.js");
 
+//just file system library
 var fs = require("fs");
 
+//try for error catching
 try {
+    //flag setting
     var isBasic = true;
 
+    //determine user entry
     if (process.argv[2].toLowerCase() === "basic") {
-        var textFile = "basicFlashCard.txt";
-    } else if (process.argv[2].toLowercase() === "cloze") {
+        var textFile = "basic.txt";
+    } else if (process.argv[2].toLowerCase() === "cloze") {
         var textFile = "clozeFlashCard.txt";
         isBasic = false;
     } else {
@@ -19,7 +24,7 @@ try {
     //invoke readFile function
     //flashcard.txt in same directory
     //utf8 decoding
-    fs.readFile("basicFlashCard.txt", "utf8", function (error, data) {
+    fs.readFile(textFile, "utf8", function (error, data) {
         if (error) throw error
 
         //assign values frm .txt file to array variable
@@ -29,21 +34,24 @@ try {
         //user Constructor to build flashcard objects
         dataArr.forEach(element => {
 
+            //split flashcard string by question/answer
             let flashcardArr = element.split(",");
 
-            console.log("FLASHCARD ===============================");
+            console.log("FLASHCARD\n=========================================");
 
+            //instantiating appropriate object based on user entry
             if (isBasic) {
                 var basicCard = new basicCardObj(flashcardArr[0], flashcardArr[1].trim());
-                basicCard.showFrontBack();
+                basicCard.showFrontBack();//print flashcard
             } else {
                 var clozeCard = new clozeCardObj(flashcardArr[0], flashcardArr[1].trim());
-                clozeCard.showCard();
+                clozeCard.showCard();//print flashcard
             }
 
             console.log("=========================================\n");
         });
     })
+    //catch all error 
 } catch (err) {
     console.log("Looks like something went wrong.\nMake sure you type 'Basic' or 'Cloze' after 'node main.js'...\nCheck if 'flashcard.txt' is named properly and is in the correct directory.")
 }
